fb7469b8aad985e51baedc6a95d06c1fa2cfac27
Erik Schjolberg <erik.schjolberg@gmail.com>
2019-06-10 09:30:30 -0500

    fresten

--- My-Blog/Resources/search.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var cx = '000888210889775888983:pqb3ch1ewhg';
-    var gcse = document.createElement('script');
-    gcse.type = 'text/javascript';
-    gcse.async = true;
-    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(gcse, s);
-  })();
